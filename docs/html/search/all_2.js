c8b37a17a7908a400e45c9257715606ddf36956d
danil-korovin <st128737@student.spbu.ru>
2025-06-05 18:32:21 +0300

    adding docs

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['labwork3_3',['LabWork3',['../md_README.html',1,'']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['circular_20list_6',['Circular List',['../md_documentation.html',1,'']]]
+];
